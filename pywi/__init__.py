755aa702f2fd0a923d2c267956b82046724cdb45
Jeremie DECOCK <jd.jdhp@gmail.com>
2018-03-22 12:03:30 +0100

    Update the __init__.py files: add packages modules in the package name space to make the package navigation more convenient in ipython and in jupyter notebooks.

--- pywi/__init__.py
+++ pywi/__init__.py
@@ -1,41 +1,3 @@
-# The MIT License
-#
-# Copyright (c) 2016 Jeremie DECOCK (http://www.jdhp.org)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-# PEP0440 compatible formatted version, see:
-# https://www.python.org/dev/peps/pep-0440/
-#
-# Generic release markers:
-# X.Y
-# X.Y.Z # For bugfix releases  
-# 
-# Admissible pre-release markers:
-# X.YaN # Alpha release
-# X.YbN # Beta release         
-# X.YrcN # Release Candidate   
-# X.Y # Final release
-#
-# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
-# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
-
 """Python Wavelet Imaging
 
 PyWI is an image filtering library aimed at removing additive background noise
@@ -82,7 +44,31 @@ The PyWI library contains:
 Note:
 
     This project is in beta stage.
+
+Viewing documentation using IPython
+-----------------------------------
+To see which functions are available in `pywi`, type ``pywi.<TAB>`` (where
+``<TAB>`` refers to the TAB key), or use ``pywi.*transform*?<ENTER>`` (where
+``<ENTER>`` refers to the ENTER key) to narrow down the list.  To view the
+docstring for a function, use ``pywi.transform?<ENTER>`` (to view the
+docstring) and ``pywi.transform??<ENTER>`` (to view the source code).
 """
+
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+# X.Y
+# X.Y.Z # For bugfix releases
+#
+# Admissible pre-release markers:
+# X.YaN # Alpha release
+# X.YbN # Beta release
+# X.YrcN # Release Candidate
+# X.Y # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 
 __version__ = '0.1.dev15'
 
@@ -96,12 +82,3 @@ from . import io
 from . import optimization
 from . import transform
 from . import ui
-
-__all__ = ['benchmark',
-           'filtering',
-           'image',
-           'io',
-           'optimization',
-           'transform',
-           'ui',
-           'get_version']
